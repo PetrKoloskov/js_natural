db9f125a99c22d36a069aea1bf55bfbaf03713a8
dmitry.panin <d.panin@skyeng.ru>
2021-09-21 21:39:45 +0300

    Get started, yyyobta

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './index.js',
+    resolve: {
+        fallback: {
+            "fs": false,
+            "path": false,
+            "os": false,
+        }
+    },
+    module: {
+        rules: [
+          { test: /\.svg$/, use: 'svg-inline-loader' },
+          { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+          { test: /\.(js)$/, use: 'babel-loader' }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+    mode: 'production',
+    target: "web",
 };
